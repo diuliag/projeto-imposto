4e95cbe2f7810c633a10483d98764b7bbe2d8ffb
Diulia Granatto <diuliagranatto@gmail.com>
2020-05-14 08:28:15 -0300

    Consolidado de renda

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
-
+    double ganhoSalario, ganhoServico, ganhoCapital, gastoMedico, gastoEducacao;
+    double impostoSalario, impostoServico, impostoCapital;
     cout << "Renda anual com salario: ";
     cin >> ganhoSalario;
 
     cout << "Renda anual com prestacao de servico: ";
-    cin >> ganhoServicos;
+    cin >> ganhoServico;
 
     cout << "Renda anual com ganho de capital: ";
     cin >> ganhoCapital;
@@ -20,6 +20,36 @@ int main()
 
     cout << "Gastos educacionais: ";
     cin >> gastoEducacao;
+
+    double salarioMensal = ganhoSalario / 12.0;
+
+
+    if (salarioMensal < 3000.0) {
+
+        impostoSalario = 0.0;
+    }
+    else if (salarioMensal < 5000.0) {
+
+        impostoSalario = ganhoSalario * 0.1;
+    }
+    else {
+
+        impostoSalario = ganhoSalario * 0.2;
+    }
+
+    impostoServico = ganhoServico * 0.15;
+
+    impostoCapital = ganhoCapital * 0.2;
+
+
+    cout << fixed << setprecision(2);
+
+
+    cout << endl << "RELATORIO DE IMPOSTO DE RENDA" << endl << endl;
+    cout << "CONSOLIDADO DE RENDA:" << endl;
+    cout << endl << "Imposto sobre salario: " << impostoSalario << endl;
+    cout << "Imposto sobre servicos: " << impostoServico << endl;
+    cout << "Imposto sobre ganho de capital: " << impostoCapital << endl;
 
 
     return 0;
